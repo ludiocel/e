646ceadd261b1a74df9dcac756b2194628a2f6c9
ludiocel <117383322+ludiocel@users.noreply.github.com>
2023-04-22 15:22:05 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,6 +8,14 @@ function teleport()
 
     local usernames = {"MilongVerrater","LuciferVerrater", "0ds7", "workera"}
     
+   local RunService = game:GetService("RunService")
+   function antiSit()
+   if game.Players.LocalPlayer.Character.Humanoid:GetState() == Enum.HumanoidStateType.Seated then 
+   game.Players.LocalPlayer.Character.Humanoid.Jump = true
+   end
+   end
+   RunService:BindToRenderStep("tempBinding", 1000, antiSit)
+   
     function chat(message)
        game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(message, "All")
     end
